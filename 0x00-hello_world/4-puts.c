2c5347c57b523eb783fd50339e2a094f9895e428
BenOchola <obonyob92@gmail.com>
2023-03-16 17:12:52 -0700

    C programing hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**
- * main entry point
- *
- * */
+
+
 int main(void)
+
 {
-	printf("Programming is like building a multilingual puzzle \n");
+	puts(""Programming is like building a multilingual puzzle");
 	Return(0);
 }
