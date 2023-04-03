e4ae4df156e999f31247a001505be5c02412ff0f
BenOchola <obonyob92@gmail.com>
2023-04-03 09:21:38 -0700

    Even more pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+
+*_memcpy - a function that copies memory area
+
+*@dest: memory where is stored
+
+*@src: memory where is copied
+
+*@n: number of bytes
+
+*
+
+*Return: copied memory with n bytes changed
+
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+
+// a function that copies memory area
+
+// dest: memory where is stored
+
+// src: memory where is copied
+
+// n: number of bytes
+
+// returns: copied memory with n bytes changed
+
+
+int i = n;
+
+
+for (int r = 0; r < i; r++)
+
+{
+
+dest[r] = src[r];
+
+}
+
+return (dest);
+
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+
+* _strchr - Entry point
+
+* @s: input
+
+* @c: input
+
+* Return: Always 0 (Success)
+
+*/
+
+char *_strchr(char *s, char c)
+
+{
+
+// Entry point
+
+// s: input string to search
+
+// c: target character to find
+
+// returns: pointer to the first occurrence of `c` in `s`, or null pointer if not found
+
+for (int i = 0; s[i] >= '\0'; i++)
+
+{
+
+if (s[i] == c)
+
+{
+
+return (&s[i]);
+
+}
+
+}
+
+return (0);
+
+}
